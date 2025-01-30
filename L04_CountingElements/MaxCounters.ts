79ace50b2b53129fb7109bf89ae26846d5d17ed2
Adrian Bona <adrianbona@outlook.com>
2025-01-29 22:21:35 -0300

    It can be done

--- L04_CountingElements/MaxCounters.ts
+++ L04_CountingElements/MaxCounters.ts
@@ -1,12 +1,28 @@
 ﻿function solution(N: number, A: number[]): number[] {
-  let length: number = A.length;
-  let counters: number[] = new Array(N).fill(0);
+    const length: number = A.length;
+    let counters: number[] = new Array<number>(N).fill(0);
 
-  let indexer: number[] = new Array(length).fill(0);
-  indexer[0] = N;
+    let newMax: boolean = false;
+    let max: number = 0;
 
-  let maxApplied: boolean = false;
-  let max: number = 0;
+    for (let i: number = 0; i < length; i++) {
+        if (A[i] > N) {
+            if (newMax) {
+                for (let j: number = 0; j < N; j++) {
+                    counters[j] = max;
+                }
+                newMax = false;
+            }
+        }
+        else {
+            let value: number = counters[A[i] - 1] + 1;
+            counters[A[i] - 1] = value;
+            if (value > max) {
+                max = value;
+            }
+            newMax = true;
+        }
+    }
 
-  return counters;
+    return counters;
 }
